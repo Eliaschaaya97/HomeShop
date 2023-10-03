3714a3380c16e57fb6ec03e3f563778470420db0
chaaya <chaaya@chaayas-MacBook-Pro.local>
2023-10-03 20:16:05 +0300

    add

--- src/App.js
+++ src/App.js
@@ -9,6 +9,24 @@ import { ShoppingcartProvider } from "./context/ShoppingCartContext";
 import Footer from "./components/Footer";
 import "./App.css"; // Import the App.css file
 function App() {
-    return (_jsx(ShoppingcartProvider, { children: _jsxs("div", { className: "site", children: [_jsx(Navbar, {}), _jsx(Container, { className: "mb-4", children: _jsxs(Routes, { children: [_jsx(Route, { path: "/", element: _jsx(Home, {}) }), _jsx(Route, { path: "/store", element: _jsx(Store, {}) }), _jsx(Route, { path: "/about", element: _jsx(About, {}) })] }) }), _jsx(Footer, {})] }) }));
+  return _jsx(ShoppingcartProvider, {
+    children: _jsxs("div", {
+      className: "site",
+      children: [
+        _jsx(Navbar, {}),
+        _jsx(Container, {
+          className: "mb-4",
+          children: _jsxs(Routes, {
+            children: [
+              _jsx(Route, { path: "/HomeShop", element: _jsx(Home, {}) }),
+              _jsx(Route, { path: "/store", element: _jsx(Store, {}) }),
+              _jsx(Route, { path: "/about", element: _jsx(About, {}) }),
+            ],
+          }),
+        }),
+        _jsx(Footer, {}),
+      ],
+    }),
+  });
 }
 export default App;
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -5,8 +5,156 @@ import storeItems from "../data/items.json";
 import "./Home.css";
 import { StoreItem } from "../components/StoreItem";
 export function Home() {
-    const featuredItemIds = [1, 6, 8, 12]; // IDs of featured items
-    // Filter store items to include only the featured items
-    const featuredItems = storeItems.filter((item) => featuredItemIds.includes(item.id));
-    return (_jsxs("div", { className: "main-container", children: [_jsx("div", { className: "text-center", children: _jsx("img", { src: "/images/mobile-phones.jpg", alt: "Mobile Phones", width: "590", height: "350" }) }), _jsx("div", { className: "home-bg", children: _jsxs(Container, { className: "home-content text-center", children: [_jsx("h1", { className: "home-title", children: "Welcome to Twins Tech" }), _jsx("p", { className: "home-description", children: "Explore our cutting-edge technology products and start shopping today!" }), _jsx(Link, { to: "/store", children: _jsx(Button, { variant: "primary", className: "home-button", children: "Shop Now" }) })] }) }), _jsxs(Container, { className: "mt-5", children: [_jsx("h2", { className: "section-title text-center", children: "Featured Products" }), _jsx(Row, { children: featuredItems.map((item) => (_jsx(Col, { xs: 12, sm: 6, md: 4, lg: 3, children: _jsx(StoreItem, { ...item }) }, item.id))) })] }), _jsxs(Container, { className: "mt-5", children: [_jsx("h2", { className: "section-title text-center", children: "Discover More" }), _jsxs(Row, { children: [_jsx(Col, { xs: 12, md: 6, lg: 4, children: _jsx(Card, { className: "mb-4 border border-light", children: _jsxs(Card.Body, { children: [_jsx(Card.Title, { children: "Product Reviews" }), _jsx(Card.Text, { children: "Read reviews and ratings from our satisfied customers. Make informed decisions before purchasing." }), _jsx(Link, { to: "/reviews", children: _jsx(Button, { variant: "dark", children: "Read Reviews" }) })] }) }) }), _jsx(Col, { xs: 12, md: 6, lg: 4, children: _jsx(Card, { className: "mb-4 border border-light ", children: _jsxs(Card.Body, { children: [_jsx(Card.Title, { children: "Tech Blog" }), _jsx(Card.Text, { children: "Explore our tech blog for the latest tech news, tips, and tutorials written by our experts." }), _jsx(Link, { to: "/blog", children: _jsx(Button, { variant: "dark", children: "Read Blog" }) })] }) }) }), _jsx(Col, { xs: 12, lg: 4, children: _jsx(Card, { className: "mb-4 border border-light", children: _jsxs(Card.Body, { children: [_jsx(Card.Title, { children: "Subscribe to Newsletter" }), _jsx(Card.Text, { children: "Stay updated with our latest product releases and exclusive offers. Subscribe to our newsletter." }), _jsx(Link, { to: "/newsletter", children: _jsx(Button, { variant: "dark", children: "Subscribe" }) })] }) }) })] })] })] }));
+  const featuredItemIds = [1, 6, 8, 12]; // IDs of featured items
+  // Filter store items to include only the featured items
+  const featuredItems = storeItems.filter((item) =>
+    featuredItemIds.includes(item.id)
+  );
+  return _jsxs("div", {
+    className: "main-container",
+    children: [
+      _jsx("div", {
+        className: "text-center",
+        children: _jsx("img", {
+          src: "/images/mobile-phones.jpg",
+          alt: "Mobile Phones",
+          width: "590",
+          height: "350",
+        }),
+      }),
+      _jsx("div", {
+        className: "home-bg",
+        children: _jsxs(Container, {
+          className: "home-content text-center",
+          children: [
+            _jsx("h1", {
+              className: "home-title",
+              children: "Welcome to Twins Tech",
+            }),
+            _jsx("p", {
+              className: "home-description",
+              children:
+                "Explore our cutting-edge technology products and start shopping today!",
+            }),
+            _jsx(Link, {
+              to: "/store",
+              children: _jsx(Button, {
+                variant: "primary",
+                className: "home-button",
+                children: "Shop Now",
+              }),
+            }),
+          ],
+        }),
+      }),
+      _jsxs(Container, {
+        className: "mt-5",
+        children: [
+          _jsx("h2", {
+            className: "section-title text-center",
+            children: "Featured Products",
+          }),
+          _jsx(Row, {
+            children: featuredItems.map((item) =>
+              _jsx(
+                Col,
+                {
+                  xs: 12,
+                  sm: 6,
+                  md: 4,
+                  lg: 3,
+                  children: _jsx(StoreItem, { ...item }),
+                },
+                item.id
+              )
+            ),
+          }),
+        ],
+      }),
+      _jsxs(Container, {
+        className: "mt-5",
+        children: [
+          _jsx("h2", {
+            className: "section-title text-center",
+            children: "Discover More",
+          }),
+          _jsxs(Row, {
+            children: [
+              _jsx(Col, {
+                xs: 12,
+                md: 6,
+                lg: 4,
+                children: _jsx(Card, {
+                  className: "mb-4 border border-light",
+                  children: _jsxs(Card.Body, {
+                    children: [
+                      _jsx(Card.Title, { children: "Product Reviews" }),
+                      _jsx(Card.Text, {
+                        children:
+                          "Read reviews and ratings from our satisfied customers. Make informed decisions before purchasing.",
+                      }),
+                      _jsx(Link, {
+                        to: "/reviews",
+                        children: _jsx(Button, {
+                          variant: "dark",
+                          children: "Read Reviews",
+                        }),
+                      }),
+                    ],
+                  }),
+                }),
+              }),
+              _jsx(Col, {
+                xs: 12,
+                md: 6,
+                lg: 4,
+                children: _jsx(Card, {
+                  className: "mb-4 border border-light ",
+                  children: _jsxs(Card.Body, {
+                    children: [
+                      _jsx(Card.Title, { children: "Tech Blog" }),
+                      _jsx(Card.Text, {
+                        children:
+                          "Explore our tech blog for the latest tech news, tips, and tutorials written by our experts.",
+                      }),
+                      _jsx(Link, {
+                        to: "/blog",
+                        children: _jsx(Button, {
+                          variant: "dark",
+                          children: "Read Blog",
+                        }),
+                      }),
+                    ],
+                  }),
+                }),
+              }),
+              _jsx(Col, {
+                xs: 12,
+                lg: 4,
+                children: _jsx(Card, {
+                  className: "mb-4 border border-light",
+                  children: _jsxs(Card.Body, {
+                    children: [
+                      _jsx(Card.Title, { children: "Subscribe to Newsletter" }),
+                      _jsx(Card.Text, {
+                        children:
+                          "Stay updated with our latest product releases and exclusive offers. Subscribe to our newsletter.",
+                      }),
+                      _jsx(Link, {
+                        to: "/newsletter",
+                        children: _jsx(Button, {
+                          variant: "dark",
+                          children: "Subscribe",
+                        }),
+                      }),
+                    ],
+                  }),
+                }),
+              }),
+            ],
+          }),
+        ],
+      }),
+    ],
+  });
 }
